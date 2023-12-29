f347aebf9fff5e31d09369632c7d463da3ca3d9d
pawelkozolub <pkozolub@gmail.com>
2023-12-30 00:42:20 +0100

    Add main method to run the script

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from datahandling.graphdata import GraphData
+from datahandling.datahandling import read_data, print_data
+
+# File localization path
+INPUT_DATA_FILE = Path('src/input_data.json')
+
+
+def main():
+    """Main file to run the script."""    
+    graph_data = GraphData(read_data(INPUT_DATA_FILE))
+    print_data(graph_data)
+
+
+if __name__ == '__main__':
+    main()

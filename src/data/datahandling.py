43e2d63ac1aaf2fa2055800df0d440ee84ca88ad
pawelkozolub <pkozolub@gmail.com>
2024-01-03 22:27:11 +0100

    Update of datahandling module functions for new DataSet class

--- src/data/datahandling.py
+++ src/data/datahandling.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from data.graphdata import GraphData
+from data.dataset import DataSet
 
 
 def read_data(file_path: Path) -> dict:
@@ -14,20 +14,18 @@ def read_data(file_path: Path) -> dict:
         raise
 
 
-def print_data(graph_data: GraphData) -> None:
+def print_data(data: DataSet) -> None:
     """Optional method that prints retrieved data to terminal screen for checking."""
-    print(f'\nSetname: {graph_data.setname}')
+    print(f'\nSetname: {data.setname}')
     
     print('\nPoints:')
-    for point in graph_data.points:
-        label = point['label']
-        x = point['x']
-        y = point['y']
-        print(f'{label}: ({x}, {y})')
+    for point in data.points:
+        print(f'{point.label}: ({point.x}, {point.y})')
 
     print('\nLines:')
-    for line in graph_data.lines:
-        label = line['label']
-        x = line['x']
-        y = line['y']
-        print(f'{label}: ({x}, {y})')
+    for line in data.lines:
+        print(f'{line.label}, lhv: {line.lhv}: ({line.x}, {line.y})')
+
+    print('\nSections:')
+    for section in data.sections:
+        print(f'{section.label}: ({section.x}, {section.y}), color: {section.color}, style: {section.style}')

fc301ec0947d21b92e47855542798758b938b6e5
pawelkozolub <pkozolub@gmail.com>
2024-01-03 22:20:44 +0100

    Line class for definition of line data

---
+++ src/data/line.py
@@ -0,0 +1,36 @@
+class Line:
+    """Definition of a line data."""
+    def __init__(self, line: dict, axes: list) -> None:
+        self._label = line['label']
+        self._lhv = line['lhv']
+        self._x = []
+        self._y = []
+        self._update_line(axes)
+
+    def _update_line(self, axes: list) -> None:
+        for axis in axes:
+            if axis['label'] == 'y_axis':
+                y_min = axis['y_min']
+                y_max = axis['y_max']
+
+                self._x.append(y_min / self._lhv * 3.6)
+                self._x.append(y_max / self._lhv * 3.6)
+
+                self._y.append(y_min)
+                self._y.append(y_max)
+
+    @property
+    def label(self) -> str:
+        return self._label
+    
+    @property
+    def lhv(self) -> float:
+        return self._lhv
+    
+    @property
+    def x(self) -> float:
+        return self._x
+    
+    @property
+    def y(self) -> float:
+        return self._y

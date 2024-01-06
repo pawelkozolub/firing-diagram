21366ccc169deac6cf05a295a85859bd7dcef24c
pawelkozolub <pkozolub@gmail.com>
2024-01-06 01:26:59 +0100

    Mod main method
    - reading input filenames from JSON
    - multiple graph creation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 from pathlib import Path
-from data.datahandling import read_data, print_data
+from data.datahandling import read_input, read_data, print_data
 from graph.graph import graph
 from data.dataset import DataSet
-
-# File localization path
-INPUT_DATA_FILE = Path('src/input_data.json')
 
 
 def main():
     """Main file to run the script."""    
-    graph_data = DataSet(read_data(INPUT_DATA_FILE))
-    print_data(graph_data)
-    graph(graph_data)
+    inputs = read_input(Path('src/input_data.json'))
+    for input in inputs:
+        graph_data = read_data(input)
+        if graph_data != None:
+            data_set = DataSet(graph_data)
+            print_data(data_set)
+            graph(data_set, input.stem)
 
 
 if __name__ == '__main__':
